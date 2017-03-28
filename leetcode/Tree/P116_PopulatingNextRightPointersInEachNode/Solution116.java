daca1886cbcb8150b0130e84b6cb377f5b2626dd
verdant <verdantyang@163.com>
2017-03-28 19:36:29 +0800

    leetcode: update java version

---
+++ leetcode/Tree/P116_PopulatingNextRightPointersInEachNode/Solution116.java
@@ -0,0 +1,28 @@
+package Tree.P116_PopulatingNextRightPointersInEachNode;
+
+import Tree.TreeLinkNode;
+
+/**
+ * @Data Structures:
+ * @Algorithms used:
+ * @Time Complexity:   O(n)
+ * @Space Complexity:  O(n)
+ */
+public class Solution116 {
+
+    public void connect(TreeLinkNode root) {
+        if (root == null)
+            return;
+        TreeLinkNode current;
+        while (root.left != null) {
+            current = root;
+            while (current != null) {
+                current.left.next = current.right;
+                if (current.next != null)
+                    current.right.next = current.next.left;
+                current = current.next;
+            }
+            root = root.left;
+        }
+    }
+}
---
+++ leetcode/Tree/P235_LowestCommonAncestorOfABinarySearchTree/Solution235.java
@@ -0,0 +1,26 @@
+package Tree.P235_LowestCommonAncestorOfABinarySearchTree;
+
+import Tree.TreeNode;
+
+/**
+ * @Data Structures:
+ * @Algorithms used:  Recursion
+ * @Time Complexity:   O(n)
+ * @Space Complexity:  O(n)
+ */
+public class Solution235 {
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        int bigger = p.val > q.val ? p.val : q.val;
+        int smaller = p.val < q.val ? p.val : q.val;
+        if (bigger == smaller)
+            return p;
+        if (root.val <= bigger && root.val >= smaller)
+            return root;
+        else if (root.val < smaller)
+            return lowestCommonAncestor(root.right, p, q);
+        else if (root.val > bigger)
+            return lowestCommonAncestor(root.left, p, q);
+        return null;
+    }
+}
